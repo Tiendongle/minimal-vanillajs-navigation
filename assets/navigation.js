aa1ce0ba0f814a68a6d766b9815a0c98faf43d49
tiendongle1 <tien@tiendongle.com>
2022-08-23 01:55:50 -0700

    [Chore] Reorganize for readability

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,20 +1,4 @@
-var root = document.documentElement;
-var navigation;
-
-async function init () {
-  const data = await getJson();
-
-  navigation = document.querySelector('nav') || NavigationFunctions.createNavigationRoot();
-  NavigationFunctions.createNavigationElements(navigation, data?.cities);
-  document.body.append(navigation);
-  // Set Initial Active State
-  if(navigation?.children) {
-    const initial = navigation?.children?.[0]; // Should be refactored to set specific city as the initial state
-    activeNavSize(initial);
-    activeElement.setActive(initial);
-  }
-};
-
+// HANDLE NAVIGATION SETUP //
 const NavigationFunctions = {
   createNavigationRoot: (id = 'navigation') => {
     const mainNav = document.createElement('nav');
@@ -23,7 +7,7 @@ const NavigationFunctions = {
   },
   createNavigationElements: (wrapper, data) => {
     if (!wrapper || !data || !Array.isArray(data)) return;
-    data.map(({label, section}, key) => {
+    data.map(({label, section}) => {
       const attributes = {
         id: section,
         role: 'button',
@@ -37,47 +21,13 @@ const NavigationFunctions = {
     });
   }
 }
-
-// HANDLE NAVIGATION SETUP //
-const getJson = async () => {
-  const data = await fetch('/assets/data/navigation.json')
-    .then(handleErrors)
-    .then(json => json)
-    .catch(error => console.log(error));
-  return data;
-}
-
-const handleErrors = (response) => {
-  if (!response.ok) {
-      throw Error(response.statusText);
-  }
-  return response.json();
-}
 // END NAVIGATION SETUP //
-
-// SET ANIMATION PROPERTIES //
-const activeNavSize = (element) => {
-  const {offsetLeft, clientWidth} = element;
-  root.style.setProperty('--activeNavSize', (clientWidth)/10);
-  root.style.setProperty('--activeNavCenter', `${offsetLeft}px`);
-}
-// END SET ANIMATION PROPERTIES
-
-const activeElement = {
-  removeActive: (element = document, className = "active", selector = ".active" ) => {
-    let activeElements = element.querySelectorAll(selector);
-    [...activeElements]?.map((e) => e.classList.remove(className));
-  },
-  setActive: (element, className = "active") => {
-    element.classList.add(className);
-  }
-}
 
 // HANDLER FUNCTIONS //
 function onLocationClickHandler (event) {
   activeElement.removeActive();
   activeElement.setActive(event.target);
-  activeNavSize(event.target);
+  activeElement.activeNavSize(event.target);
 };
 // END HANDLER FUNCTIONS //
 
@@ -86,7 +36,53 @@ function setAttributes(element, attrs) {
   Object.entries(attrs)
     .forEach(([key, value]) => element.setAttribute(key, value));
 }
+
+const handleErrors = (response) => {
+  if (!response.ok) throw Error(response.statusText);
+  return response.json();
+}
 // END HELPER FUNCTIONS
+
+const getJson = async () => {
+  const data = await fetch('/assets/data/navigation.json')
+    .then(handleErrors)
+    .then(json => json)
+    .catch(error => console.log(error));
+  return data;
+}
+
+const activeElement = {
+  removeActive: (element = document, className = "active", selector = ".active" ) => {
+    let activeElements = element.querySelectorAll(selector);
+    [...activeElements]?.map((e) => e.classList.remove(className));
+  },
+  setActive: (element, className = "active") => {
+    element.classList.add(className);
+  },
+  activeNavSize: (element) => { // SET ANIMATION PROPERTIES
+    const {offsetLeft, clientWidth} = element;
+    root.style.setProperty('--activeNavSize', (clientWidth)/10);
+    root.style.setProperty('--activeNavCenter', `${offsetLeft}px`);
+  }
+}
+
+// ---- INITIALIZATION ---- //
+async function init () {
+  const data = await getJson();
+
+  navigation = document.querySelector('nav') || NavigationFunctions.createNavigationRoot();
+  NavigationFunctions.createNavigationElements(navigation, data?.cities);
+  document.body.append(navigation);
+  // Set Initial Active State
+  if(navigation?.children) {
+    const initial = navigation?.children?.[0]; // TODO: Should be refactored to set specific city as the initial state
+    activeElement.activeNavSize(initial);
+    activeElement.setActive(initial);
+  }
+};
+
+var root = document.documentElement;
+var navigation;
 
 (function () {
   init();
