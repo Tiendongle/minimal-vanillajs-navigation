870ff853e685db89d0f8fdab2daf4b71e7670bf7
tiendongle1 <tien@tiendongle.com>
2022-08-23 05:15:33 -0700

    [Feature] Add Local Time

--- assets/navigation.js
+++ assets/navigation.js
@@ -5,12 +5,20 @@ const NavigationFunctions = {
     mainNav.setAttribute('id', id);
     return mainNav;
   },
-  createNavigationElements: (wrapper, data) => {
+  createNavigationElements: async (wrapper, data) => {
     if (!wrapper || !data || !Array.isArray(data)) return;
+    let timezones;
+    try {
+      await getTimezones().then(res => timezones = res);
+    } catch (err) {
+      console.log(err);
+    }
+
     data.map(({label, section}) => {
       const attributes = {
         id: section,
         role: 'button',
+        'data-tz': timezones.get(section),
       };
 
       let element = document.createElement('button');
@@ -19,7 +27,7 @@ const NavigationFunctions = {
       element.innerText = label;
       wrapper.appendChild(element);
     });
-  }
+  },
 }
 // END NAVIGATION SETUP //
 
@@ -28,6 +36,7 @@ function onLocationClickHandler (event) {
   activeElement.removeActive();
   activeElement.setActive(event.target);
   activeElement.activeNavSize(event.target);
+  activeElement.setTime(event.target);
 };
 // END HANDLER FUNCTIONS //
 
@@ -48,8 +57,24 @@ const getJson = async () => {
     .then(handleErrors)
     .then(json => json)
     .catch(error => console.log(error));
-  return data;
+  return await data;
 }
+
+const getTimezones = async () => {
+  // Get the timezone data
+  let TIMEZONE_DATA;
+  await fetch('/assets/data/timezones.json')
+    .then(handleErrors)
+    .then(json => TIMEZONE_DATA = json.cities)
+    .catch((e) => console.log(e));
+
+  // Transform the timezone data to a dictionary
+  const timezoneMap = new Map();
+  TIMEZONE_DATA.map(({location, tz}) => timezoneMap.set(location, tz));
+
+  return timezoneMap;
+}
+
 
 const activeElement = {
   removeActive: (element = document, className = "active", selector = ".active" ) => {
@@ -63,6 +88,17 @@ const activeElement = {
     const {offsetLeft, clientWidth} = element;
     root.style.setProperty('--activeNavSize', (clientWidth));
     root.style.setProperty('--activeNavCenter', `${offsetLeft}px`);
+  },
+  setTime: (element) => {
+    const timeOptions = {
+      hour: '2-digit',
+      minute:'2-digit',
+      hour12: false,
+      timeZone: element?.dataset?.tz,
+    };
+
+    const currentTime = new Date(Date.now()).toLocaleString([], timeOptions);
+    element.setAttribute('data-time', currentTime);
   }
 }
 
@@ -71,7 +107,7 @@ async function init () {
   const data = await getJson();
 
   navigation = document.querySelector('nav') || NavigationFunctions.createNavigationRoot();
-  NavigationFunctions.createNavigationElements(navigation, data?.cities);
+  await NavigationFunctions.createNavigationElements(navigation, data?.cities);
   document.body.append(navigation);
   // Set Initial Active State
   if(navigation?.children) {
